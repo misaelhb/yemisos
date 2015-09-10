26de4c568ff145b8addcea4a43b7fcd58a18889e
Misael <misaelhb.20@hotmail.com>
2015-09-09 23:07:27 -0500

    creando proyecto ocmpilado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yemisos::Application.config.secret_token = '8a8870a64b5100084967e4a417d1079e2ce10d956b96e22355a96452bf1f3c95d968f636aa9ef1eff958af16555619dfd90733bb9ebb203029da13da3bdf7776'
